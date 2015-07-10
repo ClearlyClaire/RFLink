3e4699d309a83d6c8c0bcaa20871d97ea36ac301
stuntteam <stuntteam@e1299e7d-0413-42e1-a370-cc220ffcdf72>
2015-07-10 10:45:42 +0000

    synchronizing
    git-svn-id: svn://svn.code.sf.net/p/rflink/svn@20 e1299e7d-0413-42e1-a370-cc220ffcdf72

--- Config/Config_01.c
+++ Config/Config_01.c
@@ -5,8 +5,6 @@
 // When needed additional plugins can be added or selected plugins can be enabled/disabled.
 // ****************************************************************************************************************************************
 #define UNIT_NODO        1 // Nodo unit number after sending a [Reset] command
-#define CLOCK         true // true = include code for Real Time Clock into the compilation.
-#define NODO_MEGA     true // DO NOT CHANGE
 
 // Translation Plugin for oversized packets due to their breaks/pause being too short between packets
 // Used for Flamingo FA500R and various others, do NOT exclude this plugin.
@@ -19,10 +17,6 @@
 // Kaku : Klik-Aan-Klik-Uit (with code wheel) aka ARC
 #define PLUGIN_002
 #define PLUGIN_002_CORE
-
-// Flamingo FA500R
-#define PLUGIN_003
-#define PLUGIN_003_CORE
 
 // NewKAKU : Klik-Aan-Klik-Uit with automatic coding aka Intertechno.
 #define PLUGIN_004
@@ -49,16 +43,20 @@
 #define PLUGIN_009_CORE
 
 // RGB Switch
-//#define PLUGIN_010
-//#define PLUGIN_010_CORE
+#define PLUGIN_010
+#define PLUGIN_010_CORE
 
-// Home Easy EU receive
+// Home Confort
+#define PLUGIN_011
+#define PLUGIN_011_CORE
+
+// Flamingo FA500R
+#define PLUGIN_012
+#define PLUGIN_012_CORE
+
+// Home Easy EU 
 #define PLUGIN_015
 #define PLUGIN_015_CORE
-
-// Home Easy EU transmit
-#define PLUGIN_016
-#define PLUGIN_016_CORE
 
 // -------------------
 // Weather sensors
@@ -110,12 +108,12 @@
 // Motion Sensors, include when needed
 // -------------------
 // Ajax Chubb Varel 433 MHz. motion sensors
-//#define PLUGIN_060
-//#define PLUGIN_060_CORE
+#define PLUGIN_060
+#define PLUGIN_060_CORE
 
-// Chinese PIR motion sensors 
-//#define PLUGIN_061
-//#define PLUGIN_061_CORE
+// Chinese PIR motion door and window sensors 
+#define PLUGIN_061
+#define PLUGIN_061_CORE
 
 // -------------------
 // Doorbells
@@ -132,20 +130,20 @@
 #define PLUGIN_072
 #define PLUGIN_072_CORE
 
-// Byron MP001 doorbell
-//#define PLUGIN_074
-//#define PLUGIN_074_CORE
+// Deltronic doorbell
+#define PLUGIN_073
+#define PLUGIN_073_CORE
+
+// RL02
+#define PLUGIN_074
+#define PLUGIN_074_CORE
 
 // -------------------
 // Smoke detectors
 // -------------------
-// Flamingo FA20 / KD101 smoke detector receive
+// Flamingo FA20 / KD101 smoke detector
 #define PLUGIN_080
 #define PLUGIN_080_CORE
-
-// Flamingo FA20 / KD101 smoke detector transmit
-#define PLUGIN_081
-#define PLUGIN_081_CORE
 
 // Mertik Maxitrol / Dru fireplace
 #define PLUGIN_082
@@ -162,18 +160,90 @@
 // Housekeeping
 // -------------------
 // Nodo Slave conversion plugin
-#define PLUGIN_250
-#define PLUGIN_250_CORE
-
-// Userevent conversion
-//#define PLUGIN_251
-//#define PLUGIN_251_CORE
+#define PLUGIN_090
+#define PLUGIN_090_CORE
 
 // Debug to show unsupported packets 
 #define PLUGIN_254
 #define PLUGIN_254_CORE
 
-//Sample plugin, do not compile => use it as template to make your own plugin
-//#define PLUGIN_255
-//#define PLUGIN_255_CORE
+// ****************************************************************************************************************************************
+// RFLink List of Plugins that have TRANSMIT functionality
+// ****************************************************************************************************************************************
+// Here are all plugins listed that are supported and used after compilation.
+// When needed additional plugins can be added or selected plugins can be enabled/disabled.
+// ****************************************************************************************************************************************
+// ------------------------------------------------------------------------
+// -- Any of the following protocols can be excluded whenever not needed --
+// ------------------------------------------------------------------------
+// Kaku : Klik-Aan-Klik-Uit (with code wheel) aka ARC
+#define PLUGIN_TX_002
+#define PLUGIN_TX_002_CORE
+
+// NewKAKU : Klik-Aan-Klik-Uit with automatic coding aka Intertechno.
+#define PLUGIN_TX_004
+#define PLUGIN_TX_004_CORE
+
+// Eurodomest 
+#define PLUGIN_TX_005
+#define PLUGIN_TX_005_CORE
+
+// Blyss
+#define PLUGIN_TX_006
+#define PLUGIN_TX_006_CORE
+
+// Conrad RSL2
+#define PLUGIN_TX_007
+#define PLUGIN_TX_007_CORE
+
+// Kambrook
+#define PLUGIN_TX_008
+#define PLUGIN_TX_008_CORE
+
+// X10 RF
+#define PLUGIN_TX_009
+#define PLUGIN_TX_009_CORE
+
+// Home Confort
+#define PLUGIN_TX_011
+#define PLUGIN_TX_011_CORE
+
+// Flamingo FA500R
+#define PLUGIN_TX_012
+#define PLUGIN_TX_012_CORE
+
+// Home Easy EU 
+#define PLUGIN_TX_015
+#define PLUGIN_TX_015_CORE
+
+// -------------------
+// Doorbells
+// -------------------
+// Select Plus (Action - Quhwa) 
+#define PLUGIN_TX_070
+#define PLUGIN_TX_070_CORE
+
+// Byron SX doorbell
+#define PLUGIN_TX_072
+#define PLUGIN_TX_072_CORE
+
+// Deltronic doorbell
+#define PLUGIN_TX_073
+#define PLUGIN_TX_073_CORE
+
+// RL02
+#define PLUGIN_TX_074
+#define PLUGIN_TX_074_CORE
+// -------------------
+// Smoke detectors
+// -------------------
+// Flamingo FA20 / KD101 smoke detector
+#define PLUGIN_TX_080
+#define PLUGIN_TX_080_CORE
+
+// Mertik Maxitrol / Dru fireplace
+#define PLUGIN_TX_082
+#define PLUGIN_TX_082_CORE
+// -------------------
+//       -=#=-
 // -------------------
